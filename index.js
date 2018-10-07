e0ab67c79709e106aba0e89db103a68b9336d59f
sudharsan <sudharsan>
2018-10-07 18:52:22 +0530

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var port= process.env.PORT || 9000 ;
+var server = http.createServer(function (req, resp) {
+    if (req.url === "/") {
+        fs.readFile("pages/MyPage.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
+    } else {
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
+});
+server.listen(port);
